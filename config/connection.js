7267777719c712f05006fb8cf84b92aebc65e83e
Chris Minassian <cminassian@live.com>
2019-09-20 22:08:31 -0700

    updated

--- config/connection.js
+++ config/connection.js
@@ -1,17 +1,16 @@
 mysql = require('mysql');
 
-module.exports = {
-	var con = mysql.createConnection({
-	  host: "localhost",
-          port: 3306,
-	  user: "root",
-	  password: "password",
-	  database: "burgers_db"
-	});
+var con = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "password",
+  database: "burgers_db"
+});
 
-	con.connect(function(err) {
-	  if (err) throw err;
-	  console.log("Connected!");
-	});
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+});
 
-};
+module.exports = con;
