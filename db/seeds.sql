014fa3739d151d19466713f823b49000df5680a1
Chris Minassian <cminassian@live.com>
2019-09-20 21:33:50 -0700

    completed assignment

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ('Cheeseburger');
+INSERT INTO burgers (burger_name) VALUES ('Ham Sandwich');
+INSERT INTO burgers (burger_name) VALUES ('Double Cheeseburger');
