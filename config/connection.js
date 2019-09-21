f2b5818fd0cc0d6a7d2af3bc5cacbc7b7a80d072
Chris Minassian <cminassian@live.com>
2019-09-21 13:35:07 -0700

    updated config files

--- config/connection.js
+++ config/connection.js
@@ -4,7 +4,7 @@ var connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
-  password: "",
+  password: "password",
   database: "burgers_db"
 });
 
@@ -16,3 +16,4 @@ connection.connect(function(err) {
   console.log("connected as id " + connection.threadId);
 });
 
+module.exports = connection;
