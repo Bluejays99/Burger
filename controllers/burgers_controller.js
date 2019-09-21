7430838892ffecefe0adacbce0f6cf14eebfa912
Chris Minassian <cminassian@live.com>
2019-09-20 21:47:10 -0700

    updated burgers_controller

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,6 +1,8 @@
-import "../models/burger.js";
-import "express";
+var express = require("express");
 
+var router = express.Router();
+
+var burger = require("../models/burger");
 
 // Create all our routes and set up logic within those routes where required.
 router.get("/", function(req, res) {
