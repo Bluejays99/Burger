d42b39bd62bb2321338ce902e75a4050248e8144
Chris Minassian <cminassian@live.com>
2019-09-26 13:12:37 -0700

    modified db/seeds., server.js, layouts files

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,7 @@
-INSERT INTO burgers (burger_name, devoured) VALUES ('Cheeseburger', false);
-INSERT INTO burgers (burger_name, devoured) VALUES ('Ham Sandwich', true);
-INSERT INTO burgers (burger_name, devoured) VALUES ('Double Cheeseburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('SIR LANSHALLOT', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('THE BURGER AND THE RYE', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('FRIED & PREJUDICE', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('SWEET DREAMS ARE MADE OF CHEESE', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('HEAVEN BURGER', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('LICENSE TO MEAT', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('SWEET BURGER OF MINE', false);
