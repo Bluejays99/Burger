c9d695eda9e8e4ee8cc8658236e31f6d56348305
Chris Minassian <cminassian@live.com>
2019-09-26 13:22:04 -0700

    modified seeds.sql

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,5 +1,5 @@
 INSERT INTO burgers (burger_name, devoured) VALUES ('SIR LANSHALLOT', false);
-INSERT INTO burgers (burger_name, devoured) VALUES ('THE BURGER AND THE RYE', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('THE BURGER AND THE RYE ', false);
 INSERT INTO burgers (burger_name, devoured) VALUES ('FRIED & PREJUDICE', false);
 INSERT INTO burgers (burger_name, devoured) VALUES ('SWEET DREAMS ARE MADE OF CHEESE', false);
 INSERT INTO burgers (burger_name, devoured) VALUES ('HEAVEN BURGER', false);
